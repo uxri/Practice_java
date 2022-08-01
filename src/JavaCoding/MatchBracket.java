112c555710a0016c043a90daae75408c112f0fe4
uxri <yuree0217@g.eulji.ac.kr>
2022-08-01 19:56:32 +0900

    [PS] MatchBracket.java file

---
+++ src/JavaCoding/MatchBracket.java
@@ -0,0 +1,27 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+public class MatchBracket {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+
+        char[] arr = a.toCharArray();
+        int opencount = 0;
+        int closecount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '(') {
+                opencount++;
+            }
+            if (arr[i] == ')') {
+                closecount++;
+            }
+        }
+        if (opencount == closecount) {
+            System.out.print("valid");
+        } else {
+            System.out.print("invalid");
+        }
+    }
+}
