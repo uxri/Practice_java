c4ad24f6c1b3adcdf81ce1ba5ad0740b30a2a555
uxri <yuree0217@g.eulji.ac.kr>
2022-08-01 13:01:13 +0900

    [CHORE] Git 연동

--- src/JavaCoding/Practice.java
+++ src/JavaCoding/Practice.java
@@ -7,44 +7,23 @@ import java.util.Scanner;
 
 public class Practice {
     public static void main(String[] args) {
+        String a = "2*((3+4)/5)6";
 
-        String word = "angel";
-        String anagram = "angle";
-
-        System.out.println("angle and eagln are anagrams : " + isitanagram(word, anagram));
-    }
-
-    public static String anagramStringSort(String word) {
-        char[] arr = word.toLowerCase().toCharArray();
-        Arrays.sort(arr);
-        return String.valueOf(arr);
-    }
-
-    public static boolean isitanagram(String word, String anagram) {
-        String sortWord = anagramStringSort(word);
-        String sortAnagram = anagramStringSort(anagram);
-
-        return sortAnagram.equals(sortWord);
+        char[] arr = a.toCharArray();
+        int opencount = 0;
+        int closecount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '(') {
+                opencount++;
+            }
+            if (arr[i] == ')') {
+                closecount++;
+            }
+        }
+        if (opencount == closecount) {
+            System.out.print("valid");
+        } else {
+            System.out.print("invalid");
+        }
     }
 }
-
-//        괄호개수문제
-//        String a = "2*((3+4)/5)6";
-//
-//        char[] arr = a.toCharArray();
-//        int opencount = 0;
-//        int closecount = 0;
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i] == '(') {
-//                opencount++;
-//            }
-//            if (arr[i] == ')') {
-//                closecount++;
-//            }
-//        }
-//        if (opencount == closecount) {
-//            System.out.print("valid");
-//        }else {
-//            System.out.print("invalid");
-//        }
-//    }
