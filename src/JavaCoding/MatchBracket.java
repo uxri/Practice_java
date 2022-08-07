396209069f9447bf6856ff70df9c43b7625e4635
uxri <yuree0217@g.eulji.ac.kr>
2022-08-07 21:04:59 +0900

    [CHORE] MatchBracket String change

--- src/JavaCoding/MatchBracket.java
+++ src/JavaCoding/MatchBracket.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class MatchBracket {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String a = scanner.nextLine();
+//        Scanner scanner = new Scanner(System.in);
+//        String a = scanner.nextLine();
+        String a = "(2+3(4+5))";
 
         char[] arr = a.toCharArray();
         int opencount = 0;
