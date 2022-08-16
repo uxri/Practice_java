197729cd2c209544d88a77167751126f571bd761
uxri <yuree0217@g.eulji.ac.kr>
2022-08-17 00:01:48 +0900

    [PS] practice 1

--- src/JavaCoding/Practice.java
+++ src/JavaCoding/Practice.java
@@ -1,5 +1,13 @@
 package JavaCoding;
+
 public class Practice {
     public static void main(String[] args) {
+        String a = "yuree";
+        String reverse = "";
+
+        for (int i = a.length() - 1; i >= 0; i--) {
+            reverse = reverse + a.charAt(i);
+        }
+        System.out.println("reverse is : " + reverse);
     }
 }
