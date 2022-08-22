bf7d0ce85771604f2ca879e6663894ce02e533bd
uxri <yuree0217@g.eulji.ac.kr>
2022-08-23 03:57:26 +0900

    [JAVA] practice Q10

--- src/JavaCoding/Q10_SmallestLargest.java
+++ src/JavaCoding/Q10_SmallestLargest.java
@@ -1,17 +1,17 @@
 package JavaCoding;
 
 public class Q10_SmallestLargest {
-    public static void main(String[] args){
-        int arr[] = new int[]{12,56,76,89,100,343,21,234};
+    public static void main(String[] args) {
+        int arr[] = new int[]{12, 56, 76, 89, 100, 343, 21, 234};
 
         int smallest = arr[0];
         int largest = arr[0];
 
-        for (int i=0; i<arr.length; i++){
-            if (smallest>arr[i]){
+        for (int i = 0; i < arr.length; i++) {
+            if (smallest > arr[i]) {
                 smallest = arr[i];
             }
-            else if (largest<arr[i]){
+            if (largest < arr[i]){
                 largest = arr[i];
             }
         }
