f83ee8a5ec8a592fd420a0d5f8fb632d7c66efc3
uxri <yuree0217@g.eulji.ac.kr>
2022-08-23 03:57:01 +0900

    [JAVA] practice Q2

--- src/JavaCoding/Q2_AnagramString.java
+++ src/JavaCoding/Q2_AnagramString.java
@@ -5,21 +5,21 @@ import java.util.Arrays;
 public class Q2_AnagramString {
     public static void main(String[] args){
         String word = "yuree";
-        String anagram = "eeyeur";
+        String anagram = "eyeur";
 
         System.out.println(anagramSort(word, anagram));
     }
-    public static String sortWord(String word){
-        char[] wordArr = word.toLowerCase().toCharArray();
-        Arrays.sort(wordArr);
 
-        return String.valueOf(wordArr);
+    public static String sortWord(String word){
+        char[] arr = word.toLowerCase().toCharArray();
+        Arrays.sort(arr);
+        return String.valueOf(arr);
     }
 
     public static Boolean anagramSort(String word, String anagram){
         String sortWord = sortWord(word);
         String sortAnagram = sortWord(anagram);
 
-        return sortAnagram.equals(sortWord);
+        return sortWord.equals(sortAnagram);
     }
 }
