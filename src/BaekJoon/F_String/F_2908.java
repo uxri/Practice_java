976705534efed1a6c6d8613e67a4defee5d98cf4
uxri <yuree0217@g.eulji.ac.kr>
2022-08-24 03:45:39 +0900

    [PS] String 2908 ps

---
+++ src/BaekJoon/F_String/F_2908.java
@@ -0,0 +1,18 @@
+package BaekJoon.F_String;
+
+import java.util.Scanner;
+
+public class F_2908 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sc.close();
+
+        a = Integer.parseInt(new StringBuilder().append(a).reverse().toString());
+        b = Integer.parseInt(new StringBuilder().append(b).reverse().toString());
+
+        System.out.println(a > b ? a : b);
+    }
+}
