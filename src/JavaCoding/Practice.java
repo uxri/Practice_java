e6fd3bdd1c0efbf87408263e0a498a4adfdc65d7
uxri <yuree0217@g.eulji.ac.kr>
2022-08-25 00:22:03 +0900

    [CHORE] chore

--- src/JavaCoding/Practice.java
+++ src/JavaCoding/Practice.java
@@ -1,7 +1,24 @@
 package JavaCoding;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Practice {
     public static void main(String[] args) {
-        String a = "(2+3(4*5))";
+        System.out.println("Apple is UniqueChar : " + hasAllUniqueChars("Apple"));
+        System.out.println("qwerty is UniqueChar : " + hasAllUniqueChars("qwerty"));
+    }
+
+    public static Boolean hasAllUniqueChars(String word) {
+        HashSet hash = new HashSet();
+
+        for(int i=0; i<word.length(); i++){
+            char c = word.charAt(i);
+            if(!hash.add(c)){
+                return false;
+            }
+        }
+        return true;
     }
 }
